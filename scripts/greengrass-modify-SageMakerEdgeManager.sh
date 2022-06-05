9d924a88f229230e50b62ed4c906a9936cadfc52
Emir Ayar <emirayar@amazon.lu>
2022-06-05 20:41:06 +0200

    jq

--- scripts/greengrass-modify-SageMakerEdgeManager.sh
+++ scripts/greengrass-modify-SageMakerEdgeManager.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ../deploy/project_config.sh
 
-LAST_DEPLOYMENT_ID=$(aws greengrassv2 list-deployments --target-arn ${THING_GROUP_ARN} | jq '.deployments' | jq '.[0].deploymentId')
+LAST_DEPLOYMENT_ID=$(aws greengrassv2 list-deployments --target-arn ${THING_GROUP_ARN} | jq -r '.deployments[0].deploymentId')
 
-DEPLOYMENT=$(aws greengrassv2 get-deployment --deployment-id ${LAST_DEPLOYMENT_ID})
-
-echo ${DEPLOYMENT}
+DEPLOYMENT=$(aws greengrassv2 get-deployment --deployment-id $LAST_DEPLOYMENT_ID)
+SAGEMAKER_COMPONENT_MERGE=$(jq -r '.["components"]["aws.greengrass.SageMakerEdgeManager"]["configurationUpdate"]["merge"]' <<< "$DEPLOYMENT") 
+echo ${SAGEMAKER_COMPONENT_MERGE}
