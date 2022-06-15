5a88a38b435216d64249f7d250794cb4d58b7a9b
Emir Ayar <emirayar@amazon.lu>
2022-06-15 18:36:37 +0200

    emir's settings

--- setup_device.sh
+++ setup_device.sh
@@ -5,17 +5,17 @@ source ./project_settings.sh
 
 PROJ_NAME='aws-cloud-demo-torizon'
 
-sshpass -p $BOARD_PWD ssh -t torizon@$BOARD_IP "\
+ssh -t torizon@$BOARD_IP "\
   rm -rf /home/torizon/greengrass-*.tar.gz && \
   (echo $BOARD_PWD | sudo -S rm -rf /greengrass) \
   "
 
-sshpass -p $BOARD_PWD scp -r \
+scp -r \
   deploy/greengrass-*.tar.gz \
   deploy/kvs* \
   torizon@$BOARD_IP:/home/torizon/
 
-sshpass -p $BOARD_PWD ssh -t torizon@$BOARD_IP "\
+ssh -t torizon@$BOARD_IP "\
   (echo $BOARD_PWD | sudo -S tar xvf greengrass-*.tar.gz ) && \
   (echo $BOARD_PWD | sudo -S tar xvf kvs-*.tar.gz ) \
   "
