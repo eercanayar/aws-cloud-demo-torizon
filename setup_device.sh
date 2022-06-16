51f29bd5c850d607de0c225eba3485fb082e81a3
Emir Ayar <emirayar@amazon.lu>
2022-06-16 12:02:48 +0200

    fixes

--- setup_device.sh
+++ setup_device.sh
@@ -4,11 +4,11 @@ set -e
 source ./project_settings.sh
 
 PROJ_NAME='aws-cloud-demo-torizon'
+# !!! MANUALLY DELETE: sudo rm -rf /home/torizon/greengrass
+# !!! MANUALLY DELETE: rm -rf kvs*
 
 ssh -t torizon@$BOARD_IP "\
-  rm -rf /home/torizon/greengrass-*.tar.gz && \
-  (echo $BOARD_PWD | sudo -S rm -rf /greengrass) \
-  "
+  rm -rf /home/torizon/greengrass-*.tar.gz"
 
 scp -r \
   deploy/greengrass-*.tar.gz \
