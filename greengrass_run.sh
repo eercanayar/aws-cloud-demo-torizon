a7cc5327a4b7b54314c2ee34e9d8898f616466b0
Emir Ayar <emirayar@amazon.lu>
2022-06-16 12:01:44 +0200

    disable rm

--- greengrass_run.sh
+++ greengrass_run.sh
@@ -5,18 +5,15 @@ source ./project_settings.sh
 
 PROJ_NAME='aws-cloud-demo-torizon'
 
-docker run --rm -it --privileged torizon/binfmt
-docker build -f greengrass-Dockerfile -t $DOCKERHUB_LOGIN/$PROJ_NAME .
-
-docker push $DOCKERHUB_LOGIN/$PROJ_NAME
 
 ssh -t torizon@$BOARD_IP "\
-  docker pull $DOCKERHUB_LOGIN/$PROJ_NAME && \
-  docker run -it --network host \
+ docker run -it --network host \
     -v /dev:/dev \
     -v /tmp:/tmp \
     -v /run/udev/:/run/udev/ \
     -v /home/torizon/greengrass/:/greengrass/ \
+    -v /home/torizon/kvs/:/kvs/ \
+    --env-file=/home/torizon/kvs-settings \
     --device-cgroup-rule='c 81:* rmw' \
     --device-cgroup-rule='c 4:* rmw' \
     --device-cgroup-rule='c 13:* rmw' \
